676f88ca3fa668513793883089b6e8a43784a7c8
joemiddie13 <joemiddie13@gmail.com>
2023-10-31 10:35:00 -0700

    horoscope

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import random
+from random import randint
 
 app = Flask(__name__)
 
@@ -135,16 +136,19 @@ def horoscope_results():
     """Shows the user the result for their chosen horoscope."""
 
     # TODO: Get the sign the user entered in the form, based on their birthday
-    horoscope_sign = ''
+    horoscope_sign = request.args.get('horoscope_sign')
+
+    user_name = request.args.get('users_name')
 
     # TODO: Look up the user's personality in the HOROSCOPE_PERSONALITIES
     # dictionary based on what the user entered
-    users_personality = ''
+    users_personality = HOROSCOPE_PERSONALITIES.get(horoscope_sign, '')
 
     # TODO: Generate a random number from 1 to 99
-    lucky_number = 0
+    lucky_number = randint(1, 99)
 
     context = {
+        'user_name': user_name,
         'horoscope_sign': horoscope_sign,
         'personality': users_personality, 
         'lucky_number': lucky_number
